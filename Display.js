739f5328492b7efe1d6a0ee28de8c1c3a5490639
tonyzl <germanantoniosiete@gmail.com>
2023-02-08 12:13:33 -0500

    update 2023f08 1213

--- Display.js
+++ Display.js
@@ -5,6 +5,15 @@ class Display{
         this.calculate=new Calculate();
         this.Avalue='';
         this.Pvalue='';
-        
+
+    }
+    insertnumber(isnumber){
+        this.Avalue=this.Avalue+isnumber;
+        this,this.printvalues();
+    }
+
+    printvalues(){
+        this.displayAvalue.textContent=this.Avalue;
+        this.displayPvalue.textContent=this.Pvalue;
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const displayPvalue = document.getElementById('previus-value');
 const displayAvalue = document.getElementById('actual-value');
-const btsNumbers = document.querySelectorAll('.number');
+const btsNumbers = document.querySelectorAll('.isnumber');
 const btsOpers = document.querySelectorAll('.operator');
 
-const calculate = new Calculate();
-console.log(calculate.toadd(2,3)); 
-console.log(calculate.tosus(2,3)); 
-console.log(calculate.todiv(2,3)); 
-console.log(calculate.tomult(2,3)); 
+const display = new Display(displayPvalue,displayAvalue);
+
+btsNumbers.forEach(boton => {
+    boton.addEventListener('click', () => display.insertnumber(boton.innerHTML));
+});
